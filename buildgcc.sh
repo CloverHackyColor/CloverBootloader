a743b73243e78422e09e54e05a505d8564a8d3bc
blusseau <blusseau@52d7a1c0-eba5-4e63-a9d7-e3186cd7c462>
2013-01-19 17:24:20 +0000

    Enhance buildgcc.sh script

--- buildgcc.sh
+++ buildgcc.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Script for GCC chainload in OS X made for EDKII
 # 
@@ -11,17 +11,18 @@
 # Xcode Tools are required
 # Script tested on "Xcode 3.2" - Snow Leopard  
 #                  "Xcode 4.1" - Lion
+#                  "Xcode 4.5" - Mountain Lion
 #
 #  
 # Created by Jadran Puharic on 1/25/12.
-# 
-# 
+# Enhanced by JrCs on 01/19/2013.
+#
 
 # GCC chainload source version 
 # here we can change source versions of tools
 #
 export BINUTILS_VERSION=binutils-2.23.1
-export GCC_VERSION=4.7.2
+export GCC_VERSION=${GCC_VERSION:-4.7.2}
 export GMP_VERSION=gmp-5.0.5
 export MPFR_VERSION=mpfr-3.1.1
 export MPC_VERSION=mpc-0.9
@@ -29,7 +30,7 @@ export MPC_VERSION=mpc-0.9
 # Change PREFIX if you want gcc and binutils 
 # installed on different place
 #
-export PREFIX=~/src/opt/local
+export PREFIX=${PREFIX:-~/src/opt/local}
 
 # Change target mode of crosscompiler for
 # IA32 and X64 - (we know that this one works best)
@@ -48,18 +49,20 @@ export BINUTILS_CONFIG="--prefix=$PREFIX  --with-sysroot=$PREFIX --disable-werro
 # You can change DIR_MAIN if u wan't gcc source downloaded 
 # in different folder. 
 #
-export DIR_MAIN=~/src
-export DIR_TOOLS=$DIR_MAIN/tools/
-export DIR_GCC=$DIR_MAIN/tools/gcc 
-export DIR_DOWNLOADS=$DIR_GCC/download
-export DIR_LOGS=$DIR_GCC/logs
+export RAMDISK_MNT_PT=/tmp/buildgcc-ramdisk
+export DIR_MAIN=${DIR_MAIN:-~/src}
+export DIR_TOOLS=${DIR_TOOLS:-$DIR_MAIN/tools}
+export DIR_GCC=${DIR_GCC:-$DIR_TOOLS/gcc}
+export DIR_BUILD=${DIR_BUILD:-$RAMDISK_MNT_PT}
+export DIR_DOWNLOADS=${DIR_DOWNLOADS:-$DIR_GCC/download}
+export DIR_LOGS=${DIR_LOGS:-$DIR_GCC/logs}
 
 ## Paths for GCC (Xcode 4.1 fix) - works with Xcode 3.2 - Xcode 4.2
 #
 export CC="/usr/bin/gcc"
 export CXX="/usr/bin/g++"
 export CPP="/usr/bin/cpp"
-export LD="/usr/bin/ld" 
+export LD="/usr/bin/ld"
 
 #export CC="/opt/local/bin/gcc-apple-4.2"
 #export CXX="/opt/local/bin/g++-apple-4.2"
@@ -72,7 +75,7 @@ export LD="/usr/bin/ld"
 export MAKEFLAGS="-j `sysctl -n hw.ncpu`"
 
 
-### Check Functions ### 
+### Check Functions ###
 
 fnCheckXcode ()
 # Function: checking installation of Xcode Tools
@@ -104,14 +107,30 @@ echo
 
 # Function: Creating directory structure for EDK
 
-[ ! -d ${DIR_MAIN} ] && mkdir ${DIR_MAIN}
-[ ! -d ${DIR_TOOLS} ] && mkdir ${DIR_TOOLS}
-[ ! -d ${DIR_GCC} ] && mkdir ${DIR_GCC}
-[ ! -d ${DIR_DOWNLOADS} ] && mkdir ${DIR_DOWNLOADS}
-[ ! -d ${DIR_LOGS} ] && mkdir ${DIR_LOGS}
-[ ! -d ${PREFIX}/include ] && mkdir ${PREFIX}/include
+[ ! -d ${DIR_MAIN} ]       && mkdir ${DIR_MAIN}
+[ ! -d ${DIR_TOOLS} ]      && mkdir ${DIR_TOOLS}
+[ ! -d ${DIR_GCC} ]        && mkdir ${DIR_GCC}
+[ ! -d ${DIR_DOWNLOADS} ]  && mkdir ${DIR_DOWNLOADS}
+[ ! -d ${DIR_LOGS} ]       && mkdir ${DIR_LOGS}
+[ ! -d ${PREFIX}/include ] && mkdir -p ${PREFIX}/include
 echo
 
+
+# RAMdisk
+function mountRamDisk() {
+    dev_ramdisk=$(mount | grep "$RAMDISK_MNT_PT" | awk '{print $1}')
+    if [ -z "$dev_ramdisk" ];then
+        echo "- Creating new RAM disk"
+        dev_ramdisk=`hdiutil attach -nomount ram://614400 | awk '{print $1}'`
+        echo
+        [ -n "$dev_ramdisk" ] && newfs_hfs -v "BuildGCC RamDisk" "$dev_ramdisk"
+        [ ! -d "$RAMDISK_MNT_PT" ] && mkdir "$RAMDISK_MNT_PT"
+        mount -t hfs "$dev_ramdisk" "$RAMDISK_MNT_PT"
+
+    fi
+    # Automatically remove RAMDISK on exit
+    trap 'echo; echo "- Ejecting RAM disk"; cd "$HOME"; umount "$RAMDISK_MNT_PT" && hdiutil detach "$dev_ramdisk"' EXIT
+}
 
 # Download #
 
@@ -143,85 +162,128 @@ fnDownloadSource ()
 }
 
 
+### Extract ###
+
+# Function to extract source tarballs
+fnExtract ()
+{
+    exec 3>&1 1>&2 # Save stdout and redirect stdout to stderr
+
+    local tarball="$1"
+    local package=${tarball%%.tar*}
+    tarball="${DIR_DOWNLOADS}/$tarball"
+    local flagfile="${DIR_GCC}/$package.extracted"
+
+    local filetype=$(file -L --brief "$tarball" | tr '[A-Z]' '[a-z]')
+    local tar_filter_option=""
+
+    case ${filetype} in # convert to lowercase
+        gzip\ *)  tar_filter_option='--gzip' ;;
+        bzip2\ *) tar_filter_option='--bzip2';;
+        lzip\ *)  tar_filter_option='--lzip' ;;
+        lzop\ *)  tar_filter_option='--lzop' ;;
+        lzma\ *)  tar_filter_option='--lzma' ;;
+        xz\ *)    tar_filter_option='--xz'   ;;
+        *tar\ archive*) tar_filter_option='';;
+        *) echo "Unrecognized file format of '$tarball'"
+           exit 1
+           ;;
+    esac
+
+    # Get the root directory from the tarball
+    local first_line=$(dd if="$tarball" bs=1024 count=256 2>/dev/null | \
+        tar -t $tar_filter_option -f - 2>/dev/null | head -1)
+    local top_level_dir=${first_line#./}  # remove leading ./
+    top_level_dir=${top_level_dir%%/*}    # keep only the top level directory
+
+    [ -z "$top_level_dir" ] && echo "Error can't extract top level dir from $tarball" && exit 1
+
+    if [[ ! -d "${DIR_GCC}/$top_level_dir" || ! -f "$flagfile" ]]; then
+        echo "-  ${package} extract..."
+        rm -rf "${DIR_GCC}/$top_level_dir" # Remove old directory if exists
+        tar -C "$DIR_GCC" -x "$tar_filter_option" -f "${tarball}" && touch "${DIR_GCC}/$package.extracted"
+        echo "-  ${package} extracted"
+
+    fi
+
+    # Restore stdout for the result and close file desciptor 3
+    exec 1>&3-
+    echo "${DIR_GCC}/$top_level_dir" # Return the full path where the tarball has been extracted
+}
+
 ### Compile ###
 
 fnCompileLibs ()
 # Function: Compiling GMP/MPFR/MPC in PREFIX location
 {
-# Compile GMP
-    cd $DIR_DOWNLOADS
-    echo
-    [ ! -f $DIR_DOWNLOADS/${GMP_VERSION}.tar.bz2.extracted ] && echo "-  ${GMP_VERSION} extract..." && tar -xf ${GMP_VERSION}.tar.bz2 > ${GMP_VERSION}.tar.bz2.extracted
-    echo "-  ${GMP_VERSION} extracted"
-#    wait
-    [ ! -d ${DIR_GCC}/$ARCH-gmp ] && mkdir ${DIR_GCC}/$ARCH-gmp 
-    [ -d ${DIR_GCC}/$ARCH-gmp ] && cd ${DIR_GCC}/$ARCH-gmp && rm -rf * 
+    # Mount RamDisk
+    mountRamDisk
+
+    # Compile GMP
+    local GMP_DIR=$(fnExtract "${GMP_VERSION}.tar.bz2")
+
+    rm -rf "${DIR_BUILD}/$ARCH-gmp"
+    mkdir -p "${DIR_BUILD}/$ARCH-gmp" && cd "${DIR_BUILD}/$ARCH-gmp"
     echo "-  ${GMP_VERSION} configure..."
-    ../download/${GMP_VERSION}/configure --prefix=$PREFIX > $DIR_LOGS/gmp.$ARCH.config.log.txt 2> /dev/null
+    "${GMP_DIR}"/configure --prefix=$PREFIX > $DIR_LOGS/gmp.$ARCH.config.log.txt 2> /dev/null
     echo "-  ${GMP_VERSION} make..."
     make 1> /dev/null 2> $DIR_LOGS/gmp.$ARCH.make.log.txt
     make install 1> $DIR_LOGS/gmp.$ARCH.install.log.txt 2> /dev/null
-#    wait
+    rm -rf "${DIR_BUILD}/$ARCH-gmp"
     echo "-  ${GMP_VERSION} installed in $PREFIX  -"
 
-# Compile MPFR
-    cd $DIR_DOWNLOADS
-    echo
-    [ ! -f $DIR_DOWNLOADS/${MPFR_VERSION}.tar.bz2.extracted ] && echo "-  ${MPFR_VERSION} extract..." && tar -xf ${MPFR_VERSION}.tar.bz2 > ${MPFR_VERSION}.tar.bz2.extracted
-    echo "-  ${MPFR_VERSION} extracted"
-#    wait
-    [ ! -d ${DIR_GCC}/$ARCH-mpfr ] && mkdir ${DIR_GCC}/$ARCH-mpfr 
-    [ -d ${DIR_GCC}/$ARCH-mpfr ] && cd ${DIR_GCC}/$ARCH-mpfr && rm -rf * 
+    # Compile MPFR
+    local MPFR_DIR=$(fnExtract "${MPFR_VERSION}.tar.bz2")
+
+    rm -rf "${DIR_BUILD}/$ARCH-mpfr"
+    mkdir -p "${DIR_BUILD}/$ARCH-mpfr" && cd "${DIR_BUILD}/$ARCH-mpfr"
     echo "-  ${MPFR_VERSION} configure..."
-    ../download/${MPFR_VERSION}/configure --prefix=$PREFIX --with-gmp=$PREFIX > $DIR_LOGS/mpfr.$ARCH.config.log.txt 2> /dev/null
+    "${MPFR_DIR}"/configure --prefix=$PREFIX --with-gmp=$PREFIX > $DIR_LOGS/mpfr.$ARCH.config.log.txt 2> /dev/null
     echo "-  ${MPFR_VERSION} make..."
     make 1> /dev/null 2> $DIR_LOGS/mpfr.$ARCH.make.log.txt
     make install 1> $DIR_LOGS/mpfr.$ARCH.install.log.txt 2> /dev/null
-#    wait
+    rm -rf "${DIR_BUILD}/$ARCH-mpfr"
     echo "-  ${MPFR_VERSION} installed in $PREFIX  -"
 
-# Compile MPC
-    cd $DIR_DOWNLOADS
-    echo
-    [ ! -f $DIR_DOWNLOADS/${MPC_VERSION}.tar.gz.extracted ] && echo "-  ${MPC_VERSION} extract..." && tar -xf ${MPC_VERSION}.tar.gz > ${MPC_VERSION}.tar.gz.extracted
-#    wait
-    echo "-  ${MPC_VERSION} extracted"
-    [ ! -d ${DIR_GCC}/$ARCH-mpc ] && mkdir ${DIR_GCC}/$ARCH-mpc 
-    [ -d ${DIR_GCC}/$ARCH-mpc ] && cd ${DIR_GCC}/$ARCH-mpc && rm -rf * 
+    # Compile MPC
+    local MPC_DIR=$(fnExtract "${MPC_VERSION}.tar.gz")
+
+    rm -rf "${DIR_BUILD}/$ARCH-mpc"
+    mkdir -p "${DIR_BUILD}/$ARCH-mpc" && cd "${DIR_BUILD}/$ARCH-mpc"
     echo "-  ${MPC_VERSION} configure..."
-    ../download/${MPC_VERSION}/configure --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX  > $DIR_LOGS/mpc.$ARCH.config.log.txt 2> /dev/null
+    "${MPC_DIR}"/configure --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX  > $DIR_LOGS/mpc.$ARCH.config.log.txt 2> /dev/null
     echo "-  ${MPC_VERSION} make..."
     make 1> /dev/null 2> $DIR_LOGS/mpc.$ARCH.make.log.txt
     make install 1> $DIR_LOGS/mpc.$ARCH.install.log.txt 2> /dev/null
-#    wait
+    rm -rf "${DIR_BUILD}/$ARCH-mpc"
     echo "-  ${MPC_VERSION} installed in $PREFIX  -"
 }
 
 fnCompileBinutils ()
 # Function: Compiling Binutils in PREFIX location
 {
-    export BUILD_BINUTILS_DIR=$DIR_GCC/$ARCH-binutils
-    cd $DIR_DOWNLOADS
-    echo
-    [ ! -f $DIR_DOWNLOADS/${BINUTILS_VERSION}.tar.bz2.extracted ] && echo "-  ${BINUTILS_VERSION} extract" && tar -xf ${BINUTILS_VERSION}.tar.bz2 > ${BINUTILS_VERSION}.tar.bz2.extracted
- #   wait
-    echo "-  ${BINUTILS_VERSION} extracted"
+    # Mount RamDisk
+    mountRamDisk
+
+    export BUILD_BINUTILS_DIR=$DIR_BUILD/$ARCH-binutils
+
+    # Extract the tarball
+    local BINUTILS_DIR=$(fnExtract "${BINUTILS_VERSION}.tar.bz2")
     
     # Check GMP/MPFR/MPC
-    [ ! -f $PREFIX/include/gmp.h ] && echo "Error: ${GMP_VERSION} not installed, check logs" && exit
+    [ ! -f $PREFIX/include/gmp.h ]  && echo "Error: ${GMP_VERSION} not installed, check logs"  && exit
     [ ! -f $PREFIX/include/mpfr.h ] && echo "Error: ${MPFR_VERSION} not installed, check logs" && exit
-    [ ! -f $PREFIX/include/mpc.h ] && echo "Error: ${MPC_VERSION} not installed, check logs" && exit
+    [ ! -f $PREFIX/include/mpc.h ]  && echo "Error: ${MPC_VERSION} not installed, check logs"  && exit
 
     # Binutils build
-    [ ! -d ${DIR_GCC}/$ARCH-binutils ] && mkdir ${DIR_GCC}/$ARCH-binutils 
-    [ -d ${DIR_GCC}/$ARCH-binutils ] && cd ${DIR_GCC}/$ARCH-binutils && rm -rf * 
+    rm -rf "${DIR_BUILD}/$ARCH-binutils"
+    mkdir -p "${DIR_BUILD}/$ARCH-binutils" && cd "${DIR_BUILD}/$ARCH-binutils"
     echo "-  ${BINUTILS_VERSION} configure..."
-    ../download/${BINUTILS_VERSION}/configure --target=$TARGET $BINUTILS_CONFIG 1> $DIR_LOGS/binutils.$ARCH.config.log.txt 2> /dev/null
-#    wait
+    "${BINUTILS_DIR}"/configure --target=$TARGET $BINUTILS_CONFIG > $DIR_LOGS/binutils.$ARCH.config.log.txt 2> /dev/null
     echo "-  ${BINUTILS_VERSION} make..."
     make all 1> /dev/null 2> $DIR_LOGS/binutils.$ARCH.make.log.txt
     make install 1> $DIR_LOGS/binutils.$ARCH.install.log.txt 2> /dev/null
-#    wait
+    rm -rf "${DIR_BUILD}/$ARCH-binutils"
     [ ! -f $PREFIX/bin/$TARGET-ld ] && echo "Error: binutils-${BINUTILS_VERSION} not installed, check logs" && exit
     echo "-  ${BINUTILS_VERSION} installed in $PREFIX  -"
 }
@@ -229,22 +291,27 @@ fnCompileBinutils ()
 fnCompileGCC ()
 # Function: Compiling GCC in PREFIX location
 {
+    # Mount RamDisk
+    mountRamDisk
+
     export PATH=$PATH:$PREFIX/bin
-    cd $DIR_DOWNLOADS
-    echo
-   [ ! -f $DIR_DOWNLOADS/gcc-${GCC_VERSION}.tar.bz2.extracted ] && echo "-  gcc-${GCC_VERSION} extract..." && tar -xf gcc-${GCC_VERSION}.tar.bz2 > gcc-${GCC_VERSION}.tar.bz2.extracted 
-    echo "-  gcc-${GCC_VERSION} extracted"
-#    wait
-    [ ! -d ${DIR_GCC}/$ARCH-gcc ] && mkdir ${DIR_GCC}/$ARCH-gcc 
-    [ -d ${DIR_GCC}/$ARCH-gcc ] && cd ${DIR_GCC}/$ARCH-gcc && rm -rf * 
+
+    # Extract the tarball
+    local GCC_DIR=$(fnExtract "gcc-${GCC_VERSION}.tar.bz2")
+
+    rm -rf "${DIR_BUILD}/$ARCH-gcc"
+    mkdir -p "${DIR_BUILD}/$ARCH-gcc" && cd "${DIR_BUILD}/$ARCH-gcc"
+
     echo "-  gcc-${GCC_VERSION} configure..."
-    ../download/gcc-${GCC_VERSION}/configure --target=$TARGET $GCC_CONFIG > $DIR_LOGS/gcc.$ARCH.config.log.txt 2> /dev/null
-#    wait
+    "${GCC_DIR}"/configure --target=$TARGET $GCC_CONFIG > $DIR_LOGS/gcc.$ARCH.config.log.txt 2> /dev/null
+
     echo "-  gcc-${GCC_VERSION} make..."
     make all-gcc 1> /dev/null 2> $DIR_LOGS/gcc.$ARCH.make.log.txt
     make install-gcc 1> $DIR_LOGS/gcc.$ARCH.install.log.txt 2> /dev/null
-#    wait
-    [ ! -f $PREFIX/bin/$TARGET-gcc ] && echo "Error: gcc-${GCC_VERSION} not installed, check logs" && exit
+
+    rm -rf "${DIR_BUILD}/$ARCH-gcc"
+
+    [ ! -x $PREFIX/bin/$TARGET-gcc ] && echo "Error: gcc-${GCC_VERSION} not installed, check logs" && exit
     echo "-  gcc-${GCC_VERSION} installed in $PREFIX  -"  
     echo
 }
