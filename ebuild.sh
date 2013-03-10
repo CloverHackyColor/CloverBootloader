6cd642c28e582f663dbb6c9774262dea029bcc84
blusseau <blusseau@52d7a1c0-eba5-4e63-a9d7-e3186cd7c462>
2013-03-10 13:16:28 +0000

    Update ebuild.sh script
    * Add option compatibles with edk II build script

--- ebuild.sh
+++ ebuild.sh
@@ -10,23 +10,25 @@
 
 # Global variables
 declare -r SELF="${0##*/}"
-declare -r NUMBER_OF_CPU=$(sysctl -n hw.ncpu)
+declare -r NUMBER_OF_CPUS=$(sysctl -n hw.ncpu)
+declare -a EDK2_BUILD_OPTIONS=
 print_option_help_wc=
 have_fmt=
 VBIOSPATCHCLOVEREFI=0
+PLATFORMFILE=
+TARGETRULE=
 
 # Default values
 export TOOLCHAIN=GCC47
 export TARGETARCH=X64
 export PLATFORM=X64
 export BUILDTARGET=RELEASE
-export CLEANARG=
+export BUILDTHREADS=$(( NUMBER_OF_CPUS + 1 ))
 export WORKSPACE=${WORKSPACE:-}
 
 # Bash options
 set -e # errexit
 set -u # Blow on unbound variable
-shopt -s nocasematch
 
 ## FUNCTIONS ##
 
@@ -77,6 +79,17 @@ print_option_help () {
   fi
 }
 
+# Add edk2 build option
+addEdk2BuildOption() {
+    EDK2_BUILD_OPTIONS=("${EDK2_BUILD_OPTIONS[@]}" $@)
+}
+
+# Add edk2 build macro
+addEdk2BuildMacro() {
+    local macro="$1"
+    addEdk2BuildOption "-D" "$macro"
+}
+
 # Check Xcode toolchain
 checkXcode () {
     if [[ ! -x /usr/bin/xcodebuild ]]; then
@@ -89,28 +102,31 @@ checkXcode () {
 usage() {
     echo "Script for building CloverEFI source on Darwin OS X"
     echo
-    printf "Usage: %s [OPTIONS]\n" "$SELF"
+    printf "Usage: %s [OPTIONS] [all|fds|genc|genmake|clean|cleanall|cleanlib|modules|libraries|run]\n" "$SELF"
     echo
     echo "Configuration:"
-    print_option_help "--clean"     "clean Clover compiled files before compilation"
-    print_option_help "--cleanall"  "clean Clover compiled files and edk2 tools before compilation"
-    print_option_help "-h, --help"  "print this message and exit"
+    print_option_help "-n THREADNUMBER" "Build the platform using multi-threaded compiler [default is number of CPUs + 1]"
+    print_option_help "-h, --help"    "print this message and exit"
     print_option_help "-v, --version" "print the version information and exit"
     echo
     echo "Toolchain:"
     print_option_help "--clang"     "use XCode Clang toolchain"
     print_option_help "--gcc"       "use unix GCC toolchain"
-    print_option_help "--gcc47"     "use GCC 4.7 toolchain"
+    print_option_help "--gcc47"     "use GCC 4.7 toolchain [Default]"
     print_option_help "--xcode"     "use XCode 3.2 toolchain"
+    print_option_help "-t TOOLCHAIN, --tagname=TOOLCHAIN" "force to use a specific toolchain"
     echo
     echo "Target:"
     print_option_help "--ia32"      "build Clover in 32-bit [boot3]"
-    print_option_help "--x64"       "build Clover in 64-bit [boot6]"
+    print_option_help "--x64"       "build Clover in 64-bit [boot6] [Default]"
     print_option_help "--x64-mcp"   "build Clover in 64-bit [boot7] using BiosBlockIO (compatible with MCP chipset)"
+    print_option_help "-a TARGETARCH, --arch=TARGETARCH" "overrides target.txt's TARGET_ARCH definition"
+    print_option_help "-p PLATFORMFILE, --platform=PLATFORMFILE" "Build the platform specified by the DSC filename argument"
+    print_option_help "-b BUILDTARGET, --buildtarget=BUILDTARGET" "using the BUILDTARGET to build the platform"
     echo
     echo "Options:"
+    print_option_help "-D MACRO, --define=MACRO" "Macro: \"Name[=Value]\"."
     print_option_help "--vbios-patch-cloverefi" "activate vbios patch in CloverEFI"
-
     echo
     echo "Report bugs to http://www.projectosx.com/forum/index.php?showtopic=2490"
 }
@@ -121,9 +137,10 @@ argument () {
   shift
 
   if [[ $# -eq 0 ]]; then
-      printf "%s: option requires an argument -- \`%s'\n" "$0" "$opt" 1>&2
+      printf "%s: option \`%s' requires an argument\n" "$0" "$opt"
       exit 1
   fi
+
   echo $1
 }
 
@@ -132,7 +149,6 @@ checkCmdlineArguments() {
     while [[ $# -gt 0 ]]; do
         local option=$1
         shift
-
         case "$option" in
             -clang  | --clang)   TOOLCHAIN=XCLANG  ;;
             -gcc47  | --gcc47)   TOOLCHAIN=GCC47   ;;
@@ -141,26 +157,60 @@ checkCmdlineArguments() {
             -ia32 | --ia32)      TARGETARCH=IA32 ; PLATFORM=Ia32  ;;
             -x64 | --x64)        TARGETARCH=X64  ; PLATFORM=X64   ;;
             -mc | --x64-mcp)     TARGETARCH=X64  ; PLATFORM=64MCP ;;
-            -clean | --clean)
-                      CLEANARG=clean ;;
-            -cleanall | --cleanall)
-                      CLEANARG=cleanall ;;
-            -d | -debug | --debug)
-                      BUILDTARGET=DEBUG ;;
-            -r | -release | --release)
-                      BUILDTARGET=RELEASE ;;
+            -clean)    TARGETRULE=clean ;;
+            -cleanall) TARGETRULE=cleanall ;;
+            -d | -debug | --debug)  BUILDTARGET=DEBUG ;;
+            -r | -release | --release) BUILDTARGET=RELEASE ;;
+            -a) TARGETARCH=$(argument $option "$@"); shift
+                ;;
+            --arch=*)
+                TARGETARCH=$(echo "$option" | sed 's/--arch=//')
+                ;;
+            -p) PLATFORMFILE=$(argument $option "$@"); shift
+                ;;
+            --platform=*)
+                PLATFORMFILE=$(echo "$option" | sed 's/--platform=//')
+                ;;
+            -b) BUILDTARGET=$(argument $option "$@"); shift
+                ;;
+            --buildtarget=*)
+                BUILDTARGET=$(echo "$option" | sed 's/--buildtarget=//')
+                ;;
+            -t) TOOLCHAIN=$(argument $option "$@"); shift
+                ;;
+            --tagname=*)
+                TOOLCHAIN=$(echo "$option" | sed 's/--tagname=//')
+                ;;
+            -D)
+                addEdk2BuildMacro $(argument $option "$@"); shift
+                ;;
+            --define=*)
+                addEdk2BuildMacro $(echo "$option" | sed 's/--define=//')
+                ;;
+            -n)
+                BUILDTHREADS=$(argument $option "$@"); shift
+                ;;
             --vbios-patch-cloverefi)
-                      VBIOSPATCHCLOVEREFI=1 ;;
+                VBIOSPATCHCLOVEREFI=1
+                ;;
             -h | -\? | -help | --help)
-                      usage && exit 0 ;;
+                usage && exit 0
+                ;;
             -v | --version)
-                      echo "$SELF v1.0" && exit 0 ;;
-            *)
+                echo "$SELF v1.0" && exit 0
+                ;;
+            -*)
                 printf "Unrecognized option \`%s'\n" "$option" 1>&2
                 exit 1
                 ;;
+            *)
+               TARGETRULE="$option"
+               ;;
         esac
     done
+
+    # Update variables
+    PLATFORMFILE="${PLATFORMFILE:-Clover/Clover${PLATFORM}.dsc}"
 }
 
 ## Check tools for the toolchain
@@ -202,9 +252,10 @@ MainBuildScript() {
     fi
 
     # Cleaning part of the script if we have told to do it
-    if [[ -n "$CLEANARG" ]]; then
-        build -p "$WORKSPACE/Clover/Clover${PLATFORM}.dsc" -a $TARGETARCH -b $BUILDTARGET -t $TOOLCHAIN -n $NUMBER_OF_CPU clean
-        [[ "$CLEANARG" == cleanall ]] && make -C $WORKSPACE/BaseTools clean
+    if [[ "$TARGETRULE" == clean || "$TARGETRULE" == cleanall ]]; then
+        build -p $PLATFORMFILE -a $TARGETARCH -b $BUILDTARGET \
+         -t $TOOLCHAIN -n $BUILDTHREADS $TARGETRULE
+        [[ "$TARGETRULE" == cleanall ]] && make -C $WORKSPACE/BaseTools clean
         exit $?
     fi
 
@@ -215,7 +266,6 @@ MainBuildScript() {
     fi
 
     # Build Clover
-    echo "Running edk2 build for Clover$PLATFORM"
     #rm $WORKSPACE/Clover/Version.h
     local clover_revision=$(cat Clover/vers.txt)
     local clover_build_date=$(date '+%Y-%m-%d %H:%M:%S')
@@ -225,12 +275,17 @@ MainBuildScript() {
     echo "#define REVISION_STR \"Clover revision: ${clover_revision}\"" >> $WORKSPACE/Clover/Version.h
     cp $WORKSPACE/Clover/Version.h $WORKSPACE/Clover/rEFIt_UEFI/
 
-    local build_options=
-    [[ "$VBIOSPATCHCLOVEREFI" -ne 0 ]] && \
-     build_options="$build_options -D ENABLE_VBIOS_PATCH_CLOVEREFI"
+    [[ "$VBIOSPATCHCLOVEREFI" -ne 0 ]] && addEdk2BuildMacro 'ENABLE_VBIOS_PATCH_CLOVEREFI'
 
-    build $build_options -p "$WORKSPACE/Clover/Clover${PLATFORM}.dsc" \
-     -a $TARGETARCH -b $BUILDTARGET -t $TOOLCHAIN -n $NUMBER_OF_CPU
+    local cmd="build ${EDK2_BUILD_OPTIONS[@]}"
+    cmd="$cmd -p $PLATFORMFILE -a $TARGETARCH -b $BUILDTARGET"
+    cmd="$cmd -t $TOOLCHAIN -n $BUILDTHREADS $TARGETRULE"
+
+    echo
+    echo "Running edk2 build for Clover$PLATFORM using the command:"
+    echo "$cmd"
+    echo
+    eval "$cmd"
 }
 
 # Deploy Clover files for packaging
