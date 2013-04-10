458accb87fe22af0d87975dbbae8c59505c5e054
slice2009 <slice2009@52d7a1c0-eba5-4e63-a9d7-e3186cd7c462>
2013-04-10 11:13:13 +0000

    SandyBridge turbo wrong detection

--- rEFIt_UEFI/Platform/cpu.c
+++ rEFIt_UEFI/Platform/cpu.c
@@ -378,6 +378,15 @@ VOID GetCPUProperties (VOID)
                 gCPUStructure.Turbo4 = (UINT16)gCPUStructure.MaxRatio;
               }                 
             }
+            
+            //Slice - we found that for some i5-2400 and i7-2600 MSR 1AD reports wrong turbo mult
+            //MSR 000001AD  0000-0000-3B3B-3B3B - from AIDA64
+            // so there is a workaround
+            if (gCPUStructure.Turbo4 == 0x3B) {
+              gCPUStructure.Turbo4 = gCPUStructure.MaxRatio + (gCPUStructure.Turbo?1:0);
+              //this correspond to 2nd-gen-core-desktop-specification-update.pdf
+            }
+            
             gCPUStructure.MaxRatio *= 10;
             gCPUStructure.Turbo1 *= 10;
             gCPUStructure.Turbo2 *= 10;
