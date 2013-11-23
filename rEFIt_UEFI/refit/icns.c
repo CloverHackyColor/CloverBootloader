977793e9f3f9aa6f71b6f8647882867c200b18ae
slice2009 <slice2009@52d7a1c0-eba5-4e63-a9d7-e3186cd7c462>
2013-11-23 08:41:15 +0000

    new icons for metal theme

--- rEFIt_UEFI/refit/icns.c
+++ rEFIt_UEFI/refit/icns.c
@@ -258,21 +258,21 @@ BUILTIN_ICON BuiltinIconTable[BUILTIN_ICON_COUNT] = {
   { NULL, L"icons\\func_secureboot_config.png",  48 },
   { NULL, L"icons\\func_reset.png",              48 },
   { NULL, L"icons\\func_shutdown.png",           48 },
-  { NULL, L"icons\\func_help.png",              128 }, //5
+  { NULL, L"icons\\func_help.png",              128 }, //7
   { NULL, L"icons\\tool_shell.png",              48 },
   { NULL, L"icons\\tool_part.png",               48 },
   { NULL, L"icons\\tool_rescue.png",             48 },
   { NULL, L"icons\\pointer.png",                 32 },
   
-  { NULL, L"icons\\vol_internal.icns",          128 }, //8+2
+  { NULL, L"icons\\vol_internal.icns",          128 }, //12
   { NULL, L"icons\\vol_external.icns",          128 },
   { NULL, L"icons\\vol_optical.icns",           128 },
-  { NULL, L"icons\\vol_firewire.icns",          128 }, //13
+  { NULL, L"icons\\vol_firewire.icns",          128 }, //15
   { NULL, L"icons\\vol_clover.icns",            128 },
   { NULL, L"icons\\vol_internal_hfs.icns",      128 },
   { NULL, L"icons\\vol_internal_ntfs.icns",     128 },
   { NULL, L"icons\\vol_internal_ext3.icns",     128 },
-  { NULL, L"icons\\vol_recovery.icns",          128 }, //18
+  { NULL, L"icons\\vol_recovery.icns",          128 }, //20
 };
 
 EG_IMAGE * BuiltinIcon(IN UINTN Id)
