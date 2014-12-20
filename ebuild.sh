206ba15dffa47d44553b7edae0eac4c4bdc70d93
blusseau <blusseau@52d7a1c0-eba5-4e63-a9d7-e3186cd7c462>
2014-12-20 15:57:28 +0000

    Add USE_APPLE_HFSPLUS_DRIVER macro
    * If USE_APPLE_HFSPLUS_DRIVER macro is defined then the HFSPlus.efi
      driver is embed into CloverEFI instead of VBoxHfs.efi driver.

--- ebuild.sh
+++ ebuild.sh
@@ -489,7 +489,6 @@ MainPostBuildScript() {
         cp -v "$BUILD_DIR_ARCH"/FSInject.efi "$CLOVER_PKG_DIR"/EFI/CLOVER/drivers32UEFI/FSInject-32.efi
         cp -v "$BUILD_DIR_ARCH"/OsxFatBinaryDrv.efi "$CLOVER_PKG_DIR"/EFI/CLOVER/drivers32UEFI/OsxFatBinaryDrv-32.efi
         cp -v "$BUILD_DIR_ARCH"/VBoxHfs.efi "$CLOVER_PKG_DIR"/EFI/CLOVER/drivers32UEFI/VBoxHfs-32.efi
-        cp -v "$BUILD_DIR_ARCH"/VBoxHfs.efi "$CLOVER_PKG_DIR"/EFI/CLOVER/drivers32/VBoxHfs-32.efi
 
         # Optional drivers
         #cp -v "${BUILD_DIR}"/${TARGETARCH}/VBoxIso9600.efi "$CLOVER_PKG_DIR"/drivers-Off/drivers32/VBoxIso9600-32.efi
@@ -553,7 +552,6 @@ MainPostBuildScript() {
 
         # Mandatory drivers
         cp -v "$BUILD_DIR_ARCH"/FSInject.efi "$CLOVER_PKG_DIR"/EFI/CLOVER/drivers64/FSInject-64.efi
-        cp -v "$BUILD_DIR_ARCH"/VBoxHfs.efi "$CLOVER_PKG_DIR"/EFI/CLOVER/drivers64/VBoxHfs-64.efi
 
         cp -v "$BUILD_DIR_ARCH"/FSInject.efi "$CLOVER_PKG_DIR"/EFI/CLOVER/drivers64UEFI/FSInject-64.efi
         cp -v "$BUILD_DIR_ARCH"/OsxFatBinaryDrv.efi "$CLOVER_PKG_DIR"/EFI/CLOVER/drivers64UEFI/OsxFatBinaryDrv-64.efi
