9c6f0fe5892131bc5bb1d76ecb21df954071a613
slice2009 <slice2009@52d7a1c0-eba5-4e63-a9d7-e3186cd7c462>
2015-03-03 07:24:43 +0000

    add script to keep edk2 folder smaller

---
+++ update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# the script to update only needed folders from edk2
+# to not keep unnecessary Arm*, Beagle, NT32, Unix...
+
+cd ../MdePkg/
+svn up
+cd ../MdeModulePkg/
+svn up
+cd ../DuetPkg/
+svn up
+cd ../EdkCompatibilityPkg/
+svn up
+cd ../IntelFrameworkModulePkg/
+svn up
+cd ../IntelFrameworkPkg/
+svn up
+cd ../IntelFrameworkPkg/
+svn up
+cd ../OvmfPkg/
+svn up
+cd ../PcAtChipsetPkg/
+svn up
+cd ../ShellPkg/
+svn up
+cd ../UefiCpuPkg/
+svn up
+cd ../BaseTools/Conf
+svn up
+cd ../Source/
+svn up
+cd ../Scripts/
+svn up
+cd ../../Clover/
+svn up
+
+echo "now you can do: cp -R Patches_for_EDK2/* ../"
