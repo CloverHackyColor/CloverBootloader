db63d14174dc4673e3566db8ecd829f47a856bdc
zenith432 <zenith432@52d7a1c0-eba5-4e63-a9d7-e3186cd7c462>
2017-04-02 19:05:37 +0000

    EFIAPI

--- rEFIt_UEFI/refit/lib.h
+++ rEFIt_UEFI/refit/lib.h
@@ -931,7 +931,7 @@ extern INTN StriCmp (
                      IN      CONST CHAR16              *SecondString
                      );
 
-extern INTN AsciiStriCmp (
+extern INTN EFIAPI AsciiStriCmp (
                           IN      CONST CHAR8              *FirstString,
                           IN      CONST CHAR8              *SecondString
                           );
