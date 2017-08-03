83be591b67fed592c0b9357355778d5d61d70ba0
sherlocks <sherlocks@52d7a1c0-eba5-4e63-a9d7-e3186cd7c462>
2017-08-03 07:53:28 +0000

    revert to HD 3000 os-info for sandy bridge desktop

--- rEFIt_UEFI/Platform/gma.c
+++ rEFIt_UEFI/Platform/gma.c
@@ -1303,17 +1303,17 @@ BOOLEAN setup_gma_devprop(pci_dt_t *gma_dev)
         DBG("  Beginning DualLink auto-detection\n");
         if (SetUGAWidth && SetUGAHeight) {
           if (UGAWidth < 1400) {
-            DBG("  Found Low Resolution Display\n");
+            DBG("  Low Resolution Display\n");
             DBG("  AAPL01,DualLink: not used\n");
           }
           else {
-            DBG("  Found High Resolution Display\n");
+            DBG("  High Resolution Display\n");
             devprop_add_value(device, "AAPL01,DualLink", (UINT8*)&DualLink, 1);
             DBG("  AAPL01,DualLink = 1\n");
           }
         }
         else {
-          DBG("  Found Unknown Resolution Display\n");
+          DBG("  Unknown Resolution Display\n");
           devprop_add_value(device, "AAPL01,DualLink", (UINT8*)&DualLink, 1);
           DBG("  AAPL01,DualLink = 1\n");
         }
@@ -1335,17 +1335,17 @@ BOOLEAN setup_gma_devprop(pci_dt_t *gma_dev)
         DBG("  Beginning DualLink auto-detection\n");
         if (SetUGAWidth && SetUGAHeight) {
           if (UGAWidth < 1400) {
-            DBG("  Found Low Resolution Display\n");
+            DBG("  Low Resolution Display\n");
             DBG("  AAPL00,DualLink: not used\n");
           }
           else {
-            DBG("  Found High Resolution Display\n");
+            DBG("  High Resolution Display\n");
             devprop_add_value(device, "AAPL00,DualLink", (UINT8*)&DualLink, 1);
             DBG("  AAPL00,DualLink = 1\n");
           }
         }
         else {
-          DBG("  Found Unknown Resolution Display\n");
+          DBG("  Unknown Resolution Display\n");
           devprop_add_value(device, "AAPL00,DualLink", (UINT8*)&DualLink, 1);
           DBG("  AAPL00,DualLink = 1\n");
         }
@@ -1825,8 +1825,8 @@ BOOLEAN setup_gma_devprop(pci_dt_t *gma_dev)
         default:
           devprop_add_value(device, "built-in", &BuiltIn, 1);
           devprop_add_value(device, "graphic-options", sandy_bridge_hd_vals[0], 4);
-          devprop_add_value(device, "AAPL,tbl-info", mba_HD3000_tbl_info, 18);
-          devprop_add_value(device, "AAPL,os-info", mba_HD3000_os_info, 20);
+          devprop_add_value(device, "AAPL,tbl-info", mn_HD3000_tbl_info, 18);
+          devprop_add_value(device, "AAPL,os-info", mn_HD3000_os_info, 20);
           break;
       }
       break;
