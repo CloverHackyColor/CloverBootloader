be9332d456d30f084af2ad86b2dc5dc442ae2472
slice2009 <slice2009@52d7a1c0-eba5-4e63-a9d7-e3186cd7c462>
2017-11-03 15:51:37 +0000

    if not SignatureFixup, it will be zeroed

--- rEFIt_UEFI/Platform/AcpiPatcher.c
+++ rEFIt_UEFI/Platform/AcpiPatcher.c
@@ -1942,6 +1942,9 @@ EFI_STATUS PatchACPI(IN REFIT_VOLUME *Volume, CHAR8 *OSVersion)
     if (GlobalConfig.SignatureFixup) {
       DBG(" SignatureFixup: 0x%x -> 0x%x\n", Facs->HardwareSignature, machineSignature);
       Facs->HardwareSignature = machineSignature;
+    } else {
+      DBG(" SignatureFixup: 0x%x -> 0\n", Facs->HardwareSignature);
+      Facs->HardwareSignature = 0;
     }
     //
     
