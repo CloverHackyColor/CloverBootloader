0600c190f1bc4788db1d77a130a3ae7d064bcd09
slice2009 <slice2009@52d7a1c0-eba5-4e63-a9d7-e3186cd7c462>
2019-07-13 19:29:59 +0000

    Version 2.5k

--- CloverPackage/package/bin/clean_po_headers.sh
+++ CloverPackage/package/bin/clean_po_headers.sh
@@ -10,7 +10,7 @@ for pofile in po/*.po; do
     lang=${pofile##*/}
     lang=${lang%.*}
     gsed -i 's/; charset=CHARSET/; charset=UTF-8/g' $pofile
-    gsed -i 's/^\"Project-Id-Version: PACKAGE VERSION/\"Project-Id-Version: Clover 2.0/' $pofile
+    gsed -i 's/^\"Project-Id-Version: PACKAGE VERSION/\"Project-Id-Version: Clover 2.5/' $pofile
     gsed -i "s/^\"Language:.*\"/\"Language: $lang\\\n\"/" $pofile
     gsed -i "s/^\"Language-Team:.*\"/\"Language-Team: $lang <$lang@li.org>\\\n\"/" $pofile
 done
