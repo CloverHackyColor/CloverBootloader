4ab2c1179f9cdbcb9423357c7d45510af382ddc1
jief <github-prf@jf-luce.name>
2020-02-23 23:26:51 +0300

    Created an Xcode project to run the C++ unit tests in Xcode console.

---
+++ Xcode/cpp_tests/cpp_tests/Platform.h
@@ -0,0 +1,41 @@
+//
+//  Platform.h.h
+//  cpp_tests
+//
+//  Created by jief on 23.02.20.
+//  Copyright © 2020 JF Knudsen. All rights reserved.
+//
+
+#ifndef Platform_h_h
+#define Platform_h_h
+
+#include <limits.h>
+#include <stdarg.h>
+
+#define UINTN unsigned long
+#define MAX_UINTN ULONG_MAX
+#define BOOLEAN bool
+#define INTN int
+
+#define IN
+
+#define VA_LIST va_list
+#define VA_START va_start
+#define VA_END va_end
+#define VA_ARG va_arg
+
+
+void CpuDeadLoop(void);
+void DebugLog(int DebugMode, const char *FormatString, ...);
+
+void* AllocatePool(UINTN  AllocationSize);
+void* ReallocatePool(UINTN  OldSize, UINTN  NewSize, void* OldBuffer);
+void FreePool(const void* Buffer);
+void CopyMem(void *Destination, void *Source, UINTN Length);
+void PauseForKey(const wchar_t* msg);
+int StrCmp(const wchar_t* FirstString, const wchar_t* SecondString);
+int StrnCmp(const wchar_t* FirstString, const wchar_t* SecondString, UINTN Length);
+int StrLen(const wchar_t* String);
+
+
+#endif /* Platform_h_h */
---
+++ Xcode/cpp_tests/cpp_tests/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  cpp_tests
+//
+//  Created by jief on 23.02.20.
+//  Copyright © 2020 JF Knudsen. All rights reserved.
+//
+
+#include <iostream>
+
+#include "../../../rEFIt_UEFI/cpp_unit_test/all_tests.h"
+
+int main(int argc, const char * argv[])
+{
+	return all_tests();
+}
