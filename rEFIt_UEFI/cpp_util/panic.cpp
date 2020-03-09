efaf94491e0acdf043b4caa331f16b803b41a514
jief <github-prf@jf-luce.name>
2020-03-09 11:09:29 +0300

    Improve panic()

--- rEFIt_UEFI/cpp_util/panic.cpp
+++ rEFIt_UEFI/cpp_util/panic.cpp
@@ -1,10 +1,19 @@
 #include "panic.h"
+#include "../Platform/Platform.h"
 
 extern "C" {
 #include <Library/BaseLib.h> // for CpuDeadLoop
 }
 
+/*
+ *
+ * If this modified, you may have to change the Qemu/gdb_launch script to adjust the breakpoint line.
+ * gdb_launch put a breakpoint at CpuDeadLoop();
+ * Currently line 18
+ */
+
 void panic(void)
 {
+	DebugLog(2, "A fatal error happened. System halted\n");
 	CpuDeadLoop();
 }
