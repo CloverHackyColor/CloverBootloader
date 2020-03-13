07f5c88a8f05becf8df0d13e90d7401e68247f6e
Sergey Isakov <isakov-sl@bk.ru>
2020-03-13 11:44:45 +0300

    fix delete fonts
    Signed-off-by: Sergey Isakov <isakov-sl@bk.ru>

--- rEFIt_UEFI/refit/main.cpp
+++ rEFIt_UEFI/refit/main.cpp
@@ -630,18 +630,21 @@ static VOID StartLoader(IN LOADER_ENTRY *Entry)
   // OSIcons
   NSVGfontChain *fontChain = fontsDB;
   while (fontChain) {
+    DBG("free fontChain \n");
     font = fontChain->font;
+    NSVGfontChain *nextChain = fontChain->next;
     if (font) {
       nsvg__deleteFont(font);
       fontChain->font = NULL;
     }
-    fontChain = fontChain->next;
+    FreePool(fontChain);
+    fontChain = nextChain->next;
   }
 //  nsvg__deleteParser(mainParser); //temporary disabled
   //destruct_globals_objects(NULL); //we can't destruct our globals here. We need, for example, Volumes.
   
   //DumpKernelAndKextPatches(Entry->KernelAndKextPatches);
-
+  DBG("start loader\n");
   // Load image into memory (will be started later)
   Status = LoadEFIImage(Entry->DevicePath, Basename(Entry->LoaderPath), NULL, &ImageHandle);
   if (EFI_ERROR(Status)) {
