60b2ed52b0ddbce4f76539de7b0fe8cc635c2895
SergeySlice <sergey.slice@gmail.com>
2022-04-29 21:20:25 +0300

    simple setNull
    Signed-off-by: SergeySlice <sergey.slice@gmail.com>

--- rEFIt_UEFI/include/Guid++.h
+++ rEFIt_UEFI/include/Guid++.h
@@ -66,7 +66,8 @@ public:
   }
   constexpr bool operator != (const GUID& other) const { return ! (*this == other); }
 
-  void setNull() { *this = GUID(); }
+//  void setNull() { *this = GUID(); }
+  void setNull() {Data1 = 0; Data2 = 0; Data3 = 0; memset( (void*)&Data4[0], 0, 8);}
   constexpr bool isNull() const { return Data1 == 0  &&  Data2 == 0  &&  Data3 == 0  &&  Data4[0] == 0  &&  Data4[1] == 0  &&  Data4[2] == 0  &&  Data4[3] == 0  &&  Data4[4] == 0  &&  Data4[5] == 0  &&  Data4[6] == 0  &&  Data4[7] == 0; }
   constexpr bool notNull() const { return !isNull(); }
   
