793e0ef05b38941566427bd41465b6d0f1cf2aa3
Slice <sergey.slice@gmail.com>
2023-06-01 22:26:08 +0300

    make build system more successful
    Signed-off-by: Slice <sergey.slice@gmail.com>

--- makeall.sh
+++ makeall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#./ebuild.sh -fr --no-lto --ia32  -D NO_GRUB_DRIVERS_EMBEDDED
-./ebuild.sh -fr -mc --no-usb -D NO_GRUB_DRIVERS_EMBEDDED
-./ebuild.sh -fr -D NO_GRUB_DRIVERS_EMBEDDED
+#./ebuild.sh -gcc53 -fr --no-lto --ia32  -D NO_GRUB_DRIVERS_EMBEDDED
+./ebuild.sh -gcc53 -fr -n 5 -mc --no-usb -D NO_GRUB_DRIVERS_EMBEDDED
+./ebuild.sh -gcc53 -fr -n 5 -D NO_GRUB_DRIVERS_EMBEDDED
 cd CloverPackage
-./makepkg
+#./makepkg
 ./makeiso
 #make iso
 cd ..
