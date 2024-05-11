ec99128dff271d4cf1ad3e06d628c38ad76f5fbe
chris1111 <leblond1111@gmail.com>
2024-05-10 20:40:36 -0400

    Update buildmtoc.sh

--- buildmtoc.sh
+++ buildmtoc.sh
@@ -16,7 +16,7 @@ OSXVER="`/usr/bin/sw_vers -productVersion | cut -d '.' -f1,2`"
 if [[ ${OSXVER} < 10.15 ]]; then
 	export CCTOOLS_VERSION=${CCTOOLS_VERSION:-cctools-994.1}
 else
-	export CCTOOLS_VERSION=${CCTOOLS_VERSION:-cctools-986}
+	export CCTOOLS_VERSION=${CCTOOLS_VERSION:-cctools-994.1}
 fi
 # Change PREFIX if you want mtoc installed on different place
 #
