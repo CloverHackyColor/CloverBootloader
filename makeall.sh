57f1a26c352c27d7e903004592667110b05f22ff
YBronst <157227982+YBronst@users.noreply.github.com>
2026-01-16 19:36:51 +0200

    gcc151 change to gcc152

--- makeall.sh
+++ makeall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./ebuild.sh -gcc151 -fr -n 5 -mc --no-usb -D NO_GRUB_DRIVERS_EMBEDDED
-./ebuild.sh -gcc151 -fr -n 5 -D NO_GRUB_DRIVERS_EMBEDDED
+./ebuild.sh -gcc152 -fr -n 5 -mc --no-usb -D NO_GRUB_DRIVERS_EMBEDDED
+./ebuild.sh -gcc152 -fr -n 5 -D NO_GRUB_DRIVERS_EMBEDDED
 cd CloverPackage
 ./makepkg >log.txt
 ./makeiso
